072360dcd5d4c3f0c565b9a793fc246299e097ac
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2024-08-29 14:15:13 +0100

    Update App Design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.2-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
