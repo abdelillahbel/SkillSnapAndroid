95f8a6908167e2f3afb9b71a581a3e7b75ccf96b
abdelillahbel <73066920+abdelillahbel@users.noreply.github.com>
2024-09-05 22:27:48 +0100

    Upgrade versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "dev.devunion.skillsnap"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.1.2"
+        versionCode = 7
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
